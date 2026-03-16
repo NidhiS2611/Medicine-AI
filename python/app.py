c00fc2f5c4f186e2d1849d5df69dc170c73ba844
Nidhi Shakya <2202401530028@sunderdeep.ac.in>
2026-03-16 12:44:37 +0530

    done

--- python/app.py
+++ python/app.py
@@ -2,16 +2,29 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
 import pandas as pd
+import os
+import gdown
 
 app = Flask(__name__)
 CORS(app)
 
-model = joblib.load("demand_model.pkl")
+# Google Drive se model download
+FILE_ID = "1wV5ifFRdzP5VFKO0730u2wWG1yhLWkQi"
+MODEL_URL = f"https://drive.google.com/uc?id={FILE_ID}"
+MODEL_PATH = "demand_model.pkl"
 
+# Model download karo agar nahi hai to
+if not os.path.exists(MODEL_PATH):
+    print("Downloading model from Google Drive...")
+    gdown.download(MODEL_URL, MODEL_PATH, quiet=False)
+    print("Download complete!")
+
+# Model load karo
+model = joblib.load(MODEL_PATH)
+print("Model loaded successfully!")
 
 @app.route("/predict", methods=["POST"])
 def predict():
-
     data = request.json
 
     medicine = data["medicine"]
@@ -21,15 +34,13 @@ def predict():
     expiry_days = int(data["expiry"])
     price = int(data["price"])
 
-    
-
     sample = pd.DataFrame({
-        "medicine":[medicine],
-        "month":[month],
-        "year":[year],
-        "stock_level":[stock],
-        "expiry_days_remaining":[expiry_days],
-        "unit_price":[price]
+        "medicine": [medicine],
+        "month": [month],
+        "year": [year],
+        "stock_level": [stock],
+        "expiry_days_remaining": [expiry_days],
+        "unit_price": [price]
     })
 
     prediction = model.predict(sample)[0]
@@ -41,23 +52,22 @@ def predict():
         risk = "EXPIRY RISK"
 
     remaining_stock = stock - predicted_sales
-
     if remaining_stock < 0:
         remaining_stock = 0
 
     loss = remaining_stock * price
 
     return jsonify({
-    "medicineName": medicine,
-    "month": month,
-    "year": year,
-    "stock": stock,
-    "expiryDays": expiry_days,
-    "unitPrice": price,
-    "predictedSales": predicted_sales,
-    "remainingStock": remaining_stock,
-    "riskLevel": risk,
-    "lossEstimate": loss
+        "medicineName": medicine,
+        "month": month,
+        "year": year,
+        "stock": stock,
+        "expiryDays": expiry_days,
+        "unitPrice": price,
+        "predictedSales": predicted_sales,
+        "remainingStock": remaining_stock,
+        "riskLevel": risk,
+        "lossEstimate": loss
     })
 
 if __name__ == "__main__":
