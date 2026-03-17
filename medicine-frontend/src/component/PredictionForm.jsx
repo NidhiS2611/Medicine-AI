14440b4a2840ccd873d2b11dec9302f64626ad61
Nidhi Shakya <2202401530028@sunderdeep.ac.in>
2026-03-17 13:48:14 +0530

    done

--- medicine-frontend/src/component/PredictionForm.jsx
+++ medicine-frontend/src/component/PredictionForm.jsx
@@ -12,7 +12,7 @@ const [price,setPrice] = useState("");
 const handleSubmit = async (e) => {
 e.preventDefault();
 
-const response = await fetch("http://127.0.0.1:5000/predict",{
+const response = await fetch("https://medicine-ai-ozai.onrender.com/predict",{
 method:"POST",
 headers:{
 "Content-Type":"application/json"
